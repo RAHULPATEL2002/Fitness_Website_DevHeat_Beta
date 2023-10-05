0a25e6b036988cfa77258e18838c410d6211f1af
RAHULPATEL2002 <146648471+RAHULPATEL2002@users.noreply.github.com>
2023-10-05 09:25:53 +0530

    Update chatboxresponse.js

--- chatbox121/chatboxresponse.js
+++ chatbox121/chatboxresponse.js
@@ -11,18 +11,45 @@ function getBotResponse(input) {
     // Simple responses
     if (input == "hello") {
         return "Hello there!";
-    } else if (input == "goodbye") {
+    }else if (input == "by") {
+        return "Talk to you later!";
+    } 
+    else if (input == "goodbye") {
+        return "Talk to you later!";
+    } 
+    else if (input == "bye") {
+        return "Talk to you later!";
+    } 
+    else if (input == "namaste") {
+        return "namaste sir";
+    } 
+    else if (input == "hi") {
+        return "hi sir please enter your name";
+    } 
+    else if (input == "hii") {
+        return "hi sir please enter your name";
+    } 
+    else if (input == "good morning") {
+        return "good morning sir";
+    } 
+    else if (input == "good afternoon") {
+        return "good after noon sir";
+    } 
+    else if (input == "good night") {
+        return "good night sir sweet dream";
+    } 
+    else if (input == "system") {
+        return "SYSTEM.........SYSYTEM.........SYSTEM";
+    } 
+    else if (input == "jai shree ram") {
+        return "jai mahakak";
+    }
+    else if (input == "radhe-radhe") {
+        return "krisna-radhe"; 
+    else if (input == "goodbye") {
         return "Talk to you later!";
     } else {
         return "Try asking something else!";
     }
 
-    
-    if (input == "madarchod") {
-        return "temi ma ki choot";
-    } else if (input == "gandu") {
-        return "Tari ma ke sath soya hu maiS";
-    } else {
-        return "Try asking something else!";
-    }
 }
